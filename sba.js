8596d14e7e18350e2bcc9fc1f6b1e3451bd7c856
Lukatami <izraylit@gmail.com>
2025-09-09 19:43:30 -0400

    Finalize task / Comments / Try-Catch added

--- sba.js
+++ sba.js
@@ -76,32 +76,36 @@ const LearnerSubmissions = [
 
 function getLearnerData(course, ag, submissions) {
   // here, we would process this data to achieve the desired result.
-  
-  if (ag.course_id !== course.id) {
-    throw new Error("Assignment group is not related to the course");
+
+  try {
+    if (ag.course_id !== course.id) {
+      throw new Error("Assignment group is not related to the course");
+    }
+
+    const result = learnerGrades();
+
+    return result;
+
+    //
+
+    // const result = [
+    //   {
+    //     id: 125,
+    //     avg: 0.985, // (47 + 150) / (50 + 150)
+    //     1: 0.94, // 47 / 50
+    //     2: 1.0, // 150 / 150
+    //   },
+    //   {
+    //     id: 132,
+    //     avg: 0.82, // (39 + 125) / (50 + 150)
+    //     1: 0.78, // 39 / 50
+    //     2: 0.833, // late: (140 - 15) / 150
+    //   },
+    // ];
+  } catch (error) {
+    console.error("Error catched: ", error.message);
+    return [];
   }
-
-  
-  const result = learnerGrades();
-
-  //
-
-  // const result = [
-  //   {
-  //     id: 125,
-  //     avg: 0.985, // (47 + 150) / (50 + 150)
-  //     1: 0.94, // 47 / 50
-  //     2: 1.0, // 150 / 150
-  //   },
-  //   {
-  //     id: 132,
-  //     avg: 0.82, // (39 + 125) / (50 + 150)
-  //     1: 0.78, // 39 / 50
-  //     2: 0.833, // late: (140 - 15) / 150
-  //   },
-  // ];
-
-  return result;
 }
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
@@ -130,6 +134,7 @@ function getActualAssignments() {
         due_at: AssignmentGroup.assignments[i].due_at,
         points_possible: AssignmentGroup.assignments[i].points_possible,
       });
+      continue;
     }
   }
   return actualAssignments;
@@ -193,44 +198,99 @@ function learnerGrades() {
 
     // Loop for every actual assignment
     for (let j = 0; j < assignmentData.length; j++) {
-
       // Declare actual assignment variable for looping
       let assignmentScore = assignmentData[j].points_possible;
       let assignmentID = assignmentData[j].id;
 
-      // 
+      // Check every submission with assignment
       if (submissionData[i].scores[assignmentID]) {
+        // Get data to use it with dates comparing
         studentData = submissionData[i].scores[assignmentID];
 
+        // Every actual assignment have positive possible points
         if (assignmentScore > 0) {
+          // Using lateSubmission function to check submittion overdue
           let finalScore = lateSubmission(studentData, assignmentData[j]);
+
+          // Calculation of the grade
           let grade = finalScore / assignmentScore;
+
+          // Round grade to max 3 digits after . if it so
           let fixedGrade = parseFloat(grade.toFixed(3));
 
+          // Add value of grade to learner result
           learnerResult[assignmentID] = fixedGrade;
 
+          // Add scores to the storage
           totalScore += finalScore;
           totalPossible += assignmentScore;
+        } else {
+          break;
         }
       }
     }
 
+    // Calculating average grade and store it
     if (totalPossible > 0) {
       learnerResult.avg = parseFloat((totalScore / totalPossible).toFixed(3));
     }
+
+    // Add all gained results to an array
     finalResults.push(learnerResult);
   }
   return finalResults;
 }
 
 function lateSubmission(submissionData, assignmentData) {
+  // Convert date strings to a real date type
   let submitted = new Date(submissionData.submitted_at);
   let due = new Date(assignmentData.due_at);
+
+  // Extract score
   let finalScore = submissionData.score;
 
+  // Main dates comparing part
   if (submitted > due) {
+    // Declare special variable to store penalty value
     let penalty = assignmentData.points_possible * 0.1;
+
+    // Calculate final score including penalty
     finalScore = Math.max(0, submissionData.score - penalty);
   }
   return finalScore;
 }
+
+// Requirement Weight
+
+// Declare variables properly using let and const where appropriate.                                5%
+
+// Use operators to perform calculations on variables and literals.                                 5%
+
+// Use strings, numbers, and Boolean values cached within variables.                                5%
+
+// Use at least two if/else statements to control program flow.
+// Optionally, use at least one switch statement.                                                   10%
+
+// Use try/catch statements to manage potential errors in the code,
+// such as incorrectly formatted or typed data being fed into your program.                         5%
+
+// Utilize at least two different types of loops.                                                   5%
+
+// Utilize at least one loop control keyword such as break or continue.                             3%
+
+// Create and/or manipulate arrays and objects.                                                     10%
+
+// Demonstrate the retrieval, manipulation,
+// and removal of items in an array or properties in an object.                                     5%
+
+// Use functions to handle repeated tasks.                                                          10%
+
+// Program outputs processed data as described above.
+// Partial credit will be earned depending on the level of adherence to the described behavior.     20%
+
+// Ensure that the program runs without errors (comment out things that do not work,
+// and explain your blockers - you can still receive partial credit).                               10%
+
+// Commit frequently to the git repository.                                                         5%
+
+// Include a README file that contains a description of your application.                           2%
